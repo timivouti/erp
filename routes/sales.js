fbd058b476431b83656105bf4e39d564f4d8e3de
timixddd <timi_v@hotmail.com>
2017-12-24 13:22:58 +0200

    Sales edit function

--- routes/sales.js
+++ routes/sales.js
@@ -43,6 +43,7 @@ function getSales(callback) {
 }
   
 
+
 router.get('/add', ensureAuthenticated, function(req, res, next) {
     res.render('addsales');
 });
@@ -86,13 +87,83 @@ router.post('/add/new', ensureAuthenticated, function(req,res) {
 
     router.get('/remove/:order_number', ensureAuthenticated, function(req,res) {
         var orderNumber = req.params.order_number;
-        console.log(orderNumber);
         var sql = "DELETE FROM sales WHERE order_number = " + orderNumber;
         connection.query(sql, function (err, result) {
             if (err) throw err;
         });
         
         res.redirect('/sales');
+    });
+
+    router.get('/edit/:order_number', ensureAuthenticated, function(req, res) {
+        var orderNumber = req.params.order_number;
+        
+
+        function getSalesEdit(callback) {
+            connection.query("SELECT * FROM sales WHERE order_number = " + orderNumber,
+                function (err, rows) {
+                    callback(err, rows); 
+                }
+            );    
+        }
+        getSalesEdit(function (err, salesResult, orderNumber){ 
+            //you might want to do something is err is not null...      
+            res.render('editsales', { 'title': 'SQL test',
+                             'result': salesResult});
+         });
+    });
+
+    router.post('/edit/:order_number', ensureAuthenticated, function(req, res) {
+        var orderNumber = req.params.order_number;
+        var order_date = req.body.order_date;
+        var customer_name = req.body.customer_name;
+        var product_code = req.body.product_code;
+        var order_amount = req.body.order_amount;
+        var order_pricewovat = req.body.order_pricewovat;
+        var order_pricevat = req.body.order_pricevat;
+        var order_totalprice = req.body.order_totalprice;
+    
+        req.checkBody('order_date', 'Order date is required').notEmpty();
+        req.checkBody('customer_name', 'Customer name is required').notEmpty();
+        req.checkBody('product_code', 'Product code is required').notEmpty();
+        req.checkBody('order_amount', 'Order amount is required').notEmpty();
+        req.checkBody('order_pricewovat', 'Order price without VAT is required').notEmpty();
+        req.checkBody('order_pricevat', 'Order VAT price is required').notEmpty();
+        req.checkBody('order_totalprice', 'Order total price is required').notEmpty();
+
+        var errors = req.validationErrors();
+
+        function getSalesEdit(callback) {
+            connection.query("SELECT * FROM sales WHERE order_number = " + orderNumber,
+                function (err, rows) {
+                    callback(err, rows); 
+                }
+            );    
+        }
+
+        if(errors) {
+        res.render('addsales', {
+            errors:errors
+            
+            });
+        return;
+        }
+
+        var SET = "SET order_date='" + order_date + "', customer_name='" + customer_name + "', product_code=" + product_code + ", order_amount=" + order_amount
+        + ", order_pricewovat=" + order_pricewovat +  ", order_vatprice=" + order_pricevat + ", order_totalprice=" + order_totalprice;
+
+
+        connection.query("UPDATE sales " + SET + " WHERE order_number=" + orderNumber);
+
+        req.flash('success_msg', 'You have edited the sale');
+
+
+
+        getSalesEdit(function (err, salesResult, orderNumber){ 
+            //you might want to do something is err is not null...      
+            res.render('editsales', { 'title': 'SQL test',
+                             'result': salesResult});
+         });
     });
 
     function ping() {
